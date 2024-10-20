007464153d2831711780471dea2488d920062148
khalilbouhlel1 <bouhlel@isikef.u-jendouba.tn>
2024-10-20 14:40:08 +0100

    update 0

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react'
+import { Card,Form,Loader } from '../components'
+
+const Home = () => {
+    const [loading, setLoading] = useState(false);
+    const [allPosts, setAllPosts] = useState(null);
+  return (
+    <section>
+        <div>
+            <h1>.3</h1>
+        </div>
+    </section>
+  )
+}
+
+export default Home
